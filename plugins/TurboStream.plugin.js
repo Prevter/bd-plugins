b0d860d78b149e95941f0d13c64407f125afa7e0
Oleksandr Nemesh <prevter@gmail.com>
2023-12-19 16:45:06 +0200

    add soundboard bypass

--- plugins/TurboStream.plugin.js
+++ plugins/TurboStream.plugin.js
@@ -1,7 +1,7 @@
 /**
  * @name TurboStream
  * @description Stream in 1080p60 without Nitro
- * @version 0.3.4
+ * @version 0.4.0
  * @author Prevter
  * @authorId 400199033915965441
  * @updateUrl https://prevter.github.io/bd-plugins/plugins/TurboStream.plugin.js
@@ -12,18 +12,21 @@ if (!global.PrevLib) throw new Error("PrevLib is required for this plugin to wor
 const config = {
     name: "TurboStream",
     description: "Stream in 1080p60 without Nitro",
-    version: "0.3.4",
-    changelog: `<p>Added update URL</p>`,
+    version: "0.4.0",
+    changelog: `<p>Added soundboard bypass</p>`,
     updateUrl: "https://prevter.github.io/bd-plugins/plugins/TurboStream.plugin.js"
 }
 
-module.exports = global.PrevLib.create(config, ([Plugin, Api]) => {
-    return {
-        start: () => {
-            Plugin.updater = setInterval(() => Api.getCurrentUser().premiumType = 2, 1000);
-        },
-        stop: () => {
-            clearInterval(Plugin.updater);
-        }
+module.exports = global.PrevLib.create(config, ([Plugin, Api]) => ({
+    start() {
+        Plugin.updater = setInterval(() => Api.getCurrentUser().premiumType = 2, 1000);
+        const style = document.createElement('style');
+        style.id = 'TurboStream';
+        style.innerHTML = `.unavailableTooltip__58709 { display: none !important; } .premiumDisabled__07ada { opacity: 1 !important; }`;
+        document.head.appendChild(style);
+    },
+    stop() {
+        clearInterval(Plugin.updater);
+        document.getElementById('TurboStream').remove();
     }
-});
+}));
